2fae21725c30f2239e9f26544aff99c6d2197c9f
ansctrwhyn <ansctrwhyn@gmail.com>
2024-09-02 11:24:15 +0700

    change repo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you today 2" }');
+    res.send('{ "response": "Hello From Thetips4you today 3" }');
 });
 
 app.get('/will', function (req, res) {
